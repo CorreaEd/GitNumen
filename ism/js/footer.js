da98c6e0cbf6ecfed1311063a103f401d90d8651
Eduardo Corrêa <correaeduardo96@outlook.com>
2022-02-18 15:25:55 -0300

    Add dos arq. do site teste
    Foram adicionados os arquivos referentes ao site teste.

---
+++ ism/js/footer.js
@@ -0,0 +1,19 @@
+nome = "Eduardo Corrêa";
+anoNascimento = " - Ano: 1996";
+facebook = "<br>Facebook: @dicasdev_oficial";
+instagram = "<br>Instagram: @dicasdev_oficial";
+twitter = "<br>Twitter: @dicasdev_oficial";
+linkedin = "<br>LinkedIn: @dicasdev_oficial";
+sexo = "<br>Sexo: Masculino";
+localizacao = "<br>Ribeirão Pires - SP";
+polo = "<br>Polo: Higienópolis";
+
+document.write(nome);
+document.write(anoNascimento);
+document.write(facebook);
+document.write(instagram);
+document.write(twitter);
+document.write(linkedin);
+document.write(sexo);
+document.write(localizacao);
+document.write(polo);
